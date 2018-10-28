148e6754ee441c087c9b6109d78196322fdf0cab
Pradeep Kumar <pradeepkumar.anvi@gmail.com>
2018-10-28 15:27:42 +0530

    uploading an image

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://shop-api:' + process.env.MONGO_ATLAS_PW + '@cluster0
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 

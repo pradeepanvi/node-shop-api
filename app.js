0fb0580c2c1ee343bf45684273464c007bb59002
Pradeep Kumar <pradeepkumar.anvi@gmail.com>
2018-10-28 17:24:22 +0530

    adding user signup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb://shop-api:' + process.env.MONGO_ATLAS_PW + '@cluster0-shard-00-00-prx0w.mongodb.net:27017,cluster0-shard-00-01-prx0w.mongodb.net:27017,cluster0-shard-00-02-prx0w.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
     useNewUrlParser: true
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');

176484be0f7f1636ee9253b54f081055ab496578
Pradeep Kumar <pradeepkumar.anvi@gmail.com>
2018-10-28 14:04:53 +0530

    populating queries

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,6 +9,7 @@ const Product = require('../models/product');
 router.get('/', (req, res, next) => {
     Order.find()
         .select('product quantity _id')
+        .populate('product','name')
         .then(docs => {
             res.status(200).json({
                 count: docs.length,
@@ -73,6 +74,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
+        .populate('product')
         .then(order => {
             if(!order) {
                 return res.status(404).json({
